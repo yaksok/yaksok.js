59e70936f6ac24b7e9a670605a70ad9e8e39e540
JongChan Choi <disjukr@naver.com>
2016-02-08 16:16:19 +0900

    reindex plugin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 export * as ast from 'ast';
 export * as compiler from 'compiler';
 export * as translator from 'translator';
-
+export * as plugin from 'plugin';
 export Parser from 'parser';
-export Plugin from 'plugin';
-
-import ConstantFolder from 'plugin/ConstantFolder';
-export var plugin = {
-    ConstantFolder
-};
 
 import { CommonContext } from 'module';
 export var context = {
