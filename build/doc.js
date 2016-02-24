2e803d842f2de47652440d24045ffa1ca73482bd
Qwaz <qwazpia@gmail.com>
2016-02-25 02:08:20 +0900

    basic document parsing

---
+++ build/doc.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import marked from 'marked';
+
+let renderer = new marked.Renderer();
+
+renderer.heading = function (text, level, raw) {
+  return '<h'
+    + level
+    + ' id="'
+    + this.options.headerPrefix
+    + raw
+    + '">'
+    + text
+    + '</h'
+    + level
+    + '>\n';
+};
+
+marked.setOptions({
+    renderer: renderer,
+    gfm: true
+});
+
+let markdownFileName = path.join(__dirname, '../gh-pages/learn.md');
+let htmlFileName = path.join(__dirname, '../gh-pages/learn.html');
+
+let markdownContent = fs.readFileSync(markdownFileName, 'utf8');
+let htmlContent = '<!DOCTYPE html>\n' +
+    '<html><head><link rel="stylesheet" type="text/css" href="css/base.css"></head>\n<body>\n<div id="content">\n' +
+    marked(markdownContent) +
+    '</div>\n</body>\n</html>\n';
+
+fs.writeFileSync(htmlFileName, htmlContent)
