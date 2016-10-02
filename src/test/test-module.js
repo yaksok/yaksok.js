7d0ae925be9376f5743eef38efe4ec7b5303267b
JongChan Choi <jong@chan.moe>
2016-10-03 04:04:59 +0900

    add regression test

--- src/test/test-module.js
+++ src/test/test-module.js
@@ -4,4 +4,7 @@ describe('모듈', _=> {
     it('의존성의 부수효과가 먼저 발생되어야 함', async () => {
         await t('module/a');
     });
+    it('약속 안에서 모듈약속 호출', async () => {
+        await t('module/call-in-yaksok');
+    });
 });
