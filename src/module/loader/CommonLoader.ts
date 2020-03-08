7b4f7c12b0c04b47bc6fe6c6f1f52f9b903afc66
Eunchong Yu <kroisse@gmail.com>
2020-03-09 01:32:10 +0900

    module에 타입 적용

--- src/module/loader/CommonLoader.ts
+++ src/module/loader/CommonLoader.ts
@@ -1,22 +1,22 @@
 import Loader from '~/module/loader/Loader';
-import { CommonContext } from '~/module/context';
+import { CommonContext, Context } from '~/module/context';
 
 export default class CommonLoader extends Loader {
-    async load(context) {
+    async load(context: Context): Promise<string> {
         if (context instanceof CommonContext) return await load(context);
         else return await super.load(context);
     }
 }
 
-async function load(commonContext) {
+async function load(commonContext: CommonContext) {
     let filePath = await CommonContext.getScriptPathFromContext(commonContext);
     return await readFile(filePath);
 }
 
-function readFile(filePath) {
+function readFile(filePath: string): Promise<string> {
     const fs = eval('require("fs-extra")');
     return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
+        fs.readFile(filePath, 'utf8', (err: any, data: any) => {
             if (err) reject(err);
             else resolve(data);
         });
