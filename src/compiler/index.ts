97da03fb555f3d25f5c29b8753c797ef2cb32b16
Eunchong Yu <kroisse@gmail.com>
2020-03-09 20:37:36 +0900

    PluginPhase 상수의 타입 보완

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,9 +1,9 @@
-export const BEFORE_RESOLVE = {} as const;
-export const AFTER_RESOLVE = {} as const;
-export const BEFORE_ANALYZE = {} as const;
-export const AFTER_ANALYZE = {} as const;
-export const BEFORE_TRANSLATE = {} as const;
-export const AFTER_TRANSLATE = {} as const;
+export const BEFORE_RESOLVE = { phase: 'BEFORE_RESOLVE' as const };
+export const AFTER_RESOLVE = { phase: 'AFTER_RESOLVE' as const };
+export const BEFORE_ANALYZE = { phase: 'BEFORE_ANALYZE' as const };
+export const AFTER_ANALYZE = { phase: 'AFTER_ANALYZE' as const };
+export const BEFORE_TRANSLATE = { phase: 'BEFORE_TRANSLATE' as const };
+export const AFTER_TRANSLATE = { phase: 'AFTER_TRANSLATE' as const };
 export type PluginPhase =
     | typeof BEFORE_RESOLVE
     | typeof AFTER_RESOLVE
